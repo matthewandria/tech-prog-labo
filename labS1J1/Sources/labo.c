e0d653c69c6e6f828a309445f28cf505cb57f939
Matthew Andria <149076090+matthewandria@users.noreply.github.com>
2024-02-08 23:09:48 -0500

    LaboS1J1

--- labS1J1/Sources/labo.c
+++ labS1J1/Sources/labo.c
@@ -1,18 +1,54 @@
 #include "labo.h"
-
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+/*
+* Faire une fonction qui retourne un sous-tableau de character de celui passer.(paramètre texte)
+*/
 char* sousTexte(char* texte, int from, int to) {
+	if (texte == NULL || from < 0 || to < 0 || from > to)
+		return NULL;
 
+	int tailleSousTexte = to - from + 1;
+	if (tailleSousTexte <= 0)
+		return NULL;
 
+	char* sousTexte = (char*)malloc((tailleSousTexte + 1) * sizeof(char));
+	if (sousTexte == NULL)
+		return NULL;
+
+	for (int i = 0; 1 < tailleSousTexte; i++) {
+		sousTexte[i] = texte[from + i];
+	}
+	sousTexte[tailleSousTexte] = '\0';
+
+	return sousTexte;
 }
-	
+
 
 
 /*
 * Faire une fonction qui copie le contenu du tableau de character dans la propriété de data
 * de la structure string.
 */
-void set_String(char* text, String* s) {
 
+typedef struct {
+	char* data;
+	int length;
+} String;
+void set_String(char* text, String* s) {
+	if (text == NULL || s == NULL)
+		return;
+
+	int length = strlen(text);
+
+	s->data = (char*)malloc((length + 1) * sizeof(char));
+	if (s->data == NULL)
+		return;
+
+	strcpy(s->data, text);
+
+	s->length = length;
 
 }
 
@@ -22,6 +58,15 @@ void set_String(char* text, String* s) {
 */
 int String_is_palindrome(String* s) {
 
+	for (int i = 0; i < sizeof(String); i++) {
+		if (s[i] == s[sizeof(String) - i]) {
+			printf("1");
+		else {
+			printf("0");
+		}
+		}
+
+	}
 
 }
 
@@ -30,6 +75,21 @@ int String_is_palindrome(String* s) {
 */
 void bubbleSort(int elements[], int n) {
 
+	bool échanger = false;
+
+	for (int i = 0; i <= sizeof(elements) - 1; i++)
+	{
+		if (elements[i] > elements[n + 1])
+		{
+			bool échanger = true;
+			elements[i] = elements[n + 1];
+			elements[i + 1] = elements[n];
+		}
+		if (i >= sizeof(elements) - 1 && échanger == true) {
+			bool échanger = false;
+			i--;
+		}
+	}
 
 }
 
